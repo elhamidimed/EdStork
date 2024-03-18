bed5c5e0c808ef8ef743e88357ef668d7355bb88
EL HAMIDI Mohammed <melhamidi@murex.com>
2024-03-18 02:11:36 +0100

    implemented the authentification page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,6 +5,7 @@ import { Icons } from "./icons";
 import myImage from "../imcons/apprendre.png";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { GiShoebillStork } from "react-icons/gi";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
 
 import {
   Navbar,
@@ -44,11 +45,13 @@ const NavBar = () => {
               Log In &crarr;{" "}
             </Button>
           </Link>
-          <Link href="#">
-            <Button className="bg-White-500 text-black font-bold text-base border-2 border-b-4 border-black hover:shadow-md hover:border-b-2 hover:bg-slate-50 py-[10px] px-[25px]">
-              Sign up{" "}
-            </Button>
-          </Link>
+          <SignUpButton mode="modal">
+            <Link href="#">
+              <Button className="bg-White-500 text-black font-bold text-base border-2 border-black hover:shadow-md hover:bg-slate-50 py-[10px] px-[25px]">
+                Sign up{" "}
+              </Button>
+            </Link>
+          </SignUpButton>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
